73dd5755b5c7409b1ce945005846eb2812c53d7c
aeneasr <3372410+aeneasr@users.noreply.github.com>
2024-05-24 11:20:18 +0200

    test: remove unused test

--- selfservice/flow/login/strategy_form_hydrator_test.go
+++ selfservice/flow/login/strategy_form_hydrator_test.go
@@ -11,26 +11,3 @@ func TestWithIdentityHint(t *testing.T) {
 	opts := NewFormHydratorOptions([]FormHydratorModifier{WithIdentityHint(expected)})
 	assert.Equal(t, expected, opts.IdentityHint)
 }
-
-func TestWithAccountEnumerationBucket(t *testing.T) {
-	opts := NewFormHydratorOptions([]FormHydratorModifier{})
-	for _, c := range identity.AllCredentialTypes {
-		assert.Falsef(t, opts.BucketShowsCredential(c), "expected false for %s", c)
-	}
-
-	opts = NewFormHydratorOptions([]FormHydratorModifier{WithAccountEnumerationBucket("hello@ory.sh")})
-	found := 0
-	var foundType identity.CredentialsType
-	for _, c := range identity.AllCredentialTypes {
-		c := c
-		if opts.BucketShowsCredential(c) {
-			foundType = c
-			found++
-		}
-	}
-
-	assert.Equal(t, 1, found, "expected exactly one to be true")
-
-	opts = NewFormHydratorOptions([]FormHydratorModifier{WithAccountEnumerationBucket("hello@ory.sh")})
-	assert.Truef(t, opts.BucketShowsCredential(foundType), "expected true for %s because bucket should be stable", foundType)
-}
